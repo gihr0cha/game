8a22920aac7decda91f895bf454783950d101cf7
gihr0cha <giovana.rocha@estudante.ifb.edu.br>
2022-05-27 23:00:40 -0300

    status10

--- Monstrinho.java
+++ Monstrinho.java
@@ -15,14 +15,13 @@ public class Monstrinho {
 	//Construtor da classe Monstrinho
 	//Os atributos do Monstrinho iniciam com valor 5
 	public Monstrinho() {
-		this.saude = 5;
-		this.energia = 5;
-		this.forca = 5;
-		this.felicidade = 5;
+		this.saude = 10;
+		this.energia = 10;
+		this.forca = 10;
+		this.felicidade = 10;
 	}
 	
 	public void passarTurno() {
-		System.out.println("Seu monstrinho est� ficando velho!");
 		//A classe GeradorDeNumeros � utilizada para gerar um n�mero aleat�rio
 		//entre 0 e 2 para ser reduzido dos atributos do Monstrinho
 		this.saude -= GeradorDeNumeros.gerar();
@@ -52,18 +51,18 @@ public class Monstrinho {
 	
 	public void status() {
 		//Este m�todo imprime os valores dos atributos do Monstrinho
-		if (this.saude <= 2 && this.energia <= 2 && this.forca <= 2 && this.felicidade<= 2) {
+		if (this.saude <= 3 && this.energia <= 3 && this.forca <= 3 && this.felicidade<= 3) {
 			System.out.println("!!!");
 		}
 		System.out.println("Dados do monstrinho:");
 		System.out.println("Sa�de: " + this.saude);
-		if(this.saude <= 2){System.out.println("Seu mostrinho est� doente...");}
+		if(this.saude <= 3){System.out.println("Seu mostrinho est� doente...");}
 		System.out.println("Energia: " + this.energia);
-		if(this.energia <= 2){System.out.println("Seu mostrinho est� cansado...");}
+		if(this.energia <= 3){System.out.println("Seu mostrinho est� cansado...");}
 		System.out.println("For�a: " + this.forca);
-		if(this.forca <= 2){System.out.println("Seu mostrinho est� fraco...");}
+		if(this.forca <= 3){System.out.println("Seu mostrinho est� fraco...");}
 		System.out.println("Felicidade: " + this.felicidade);
-		if(this.felicidade <= 2){System.out.println("Seu mostrinho est� triste...");}
+		if(this.felicidade <= 3){System.out.println("Seu mostrinho est� triste...");}
 
 
 	}
